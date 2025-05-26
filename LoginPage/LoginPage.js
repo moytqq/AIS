89d300b44e784d6dca3ec86d05a3d91017526c39
moytqq <artyom.beliaev@gmail.com>
2025-05-26 23:47:27 +0300

    Auth added

--- LoginPage/LoginPage.js
+++ LoginPage/LoginPage.js
@@ -3,20 +3,27 @@ document.getElementById('form_login').addEventListener('submit', e => {
     
     const data = {
         userName: document.getElementById('id_userLogin').value,
-        userPassword: document.getElementById('id_userPassword').value,
-        twoFactorCode: "",
-        twoFactorRecoveryCode: ""
+        password: document.getElementById('id_userPassword').value,
+        // twoFactorCode: "",
+        // twoFactorRecoveryCode: ""
     }
     
     sendLoginForm(data);
 })
 
 async function sendLoginForm(data) {
-    const res = await fetch('/api/Users/Login', {
+    const res = await fetch('https://localhost:7169/api/Users/Login', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(data)
     });
 
     const result = await res.json();
+    if (res.status === 200){
+        Cookies.set('.AspNetCore.Identity.Application', result.accessToken);
+        window.location.href = "/ProfileTeacherPage/ProfileTeacherPage.html"
+    }
+    else {
+        alert('Что-то пошло не так');
+    }
 }
