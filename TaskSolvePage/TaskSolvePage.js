8a3b240bae5be8484a04cc848274ed4bb91b191e
moytqq <artyom.beliaev@gmail.com>
2025-06-02 19:03:51 +0300

    api/Users/Logout

---
+++ TaskSolvePage/TaskSolvePage.js
@@ -0,0 +1,19 @@
+document.getElementById('profile-tooltip__button-logout').addEventListener('click', e => {
+    e.preventDefault();
+
+    Logout();
+})
+
+async function Logout() {
+    const authtoken = Cookies.get('.AspNetCore.Identity.Application');
+    const res = await fetch(`${apiHost}/Users/Logout`, {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${authtoken}`
+        }
+    });
+
+    if (res.status === 200) {
+        window.location.href = "/LoginPage/LoginPage.html";
+    }
+}
